dfb7fb0ec1faa7bfe337c96e55d443759f12a8dc
mrpapercut <947626+mrpapercut@users.noreply.github.com>
2023-07-24 20:32:52 +0200

    Improved code

--- katas/snakes-and-snakes/typescript/src/SnakesAndSnakes.ts
+++ katas/snakes-and-snakes/typescript/src/SnakesAndSnakes.ts
@@ -83,11 +83,6 @@ class SnakesAndSnakes {
         this.game.players.forEach(player => {
             this.logAction(`${player.name} moved ${player.totalSteps} total spaces and hit ${player.totalSnakes} snakes. Final position: ${player.currentPosition}`)
         });
-
-        // const totalSnakes = this.game.players.reduce((prev, curr) => {
-        //     return prev + curr.totalSnakes;
-        // }, 0);
-        // console.log({ rounds: this.game.rounds, winner: this.game.winner, totalSnakes })
     }
 
     private roll(): number {
@@ -108,61 +103,97 @@ class SnakesAndSnakes {
 
     private handlePlayerTurn(playerIndex: number): void {
         const player = this.game.players[playerIndex];
-
-        this.logAction(`${player.name}'s turn`);
-
         const roll = this.roll();
 
-        this.logAction(`${player.name} rolled a ${roll}`);
+        this.logAction(`${player.name}'s turn and they rolled a ${roll}.${roll === 1 ? ' Tough.' : roll === 6 ? ' Great!' : ''}`);
 
         if (player.isOnBoard === false) {
-            if (roll === 6) {
-                this.logAction(`${player.name} is on board!`);
-
-                this.game.players[playerIndex].currentPosition = 1
-                this.game.players[playerIndex].isOnBoard = true
-            } else {
-                this.logAction(`${player.name} failed to get on board`);
-            }
-        } else {
-            this.logAction(`${player.name} was on ${player.currentPosition}, will move to ${player.currentPosition + roll}`)
-
-            let newPosition = player.currentPosition + roll;
-            this.game.players[playerIndex].totalSteps += roll
-
-            if (newPosition === 100) {
-                this.logAction(`${player.name} has won!!!`);
-
-                this.game.players[playerIndex].currentPosition = newPosition
-                this.game.gameIsOver = true;
-                this.game.winner = playerIndex;
-
-                return;
-            }
-
-            if (newPosition > 100) {
-                this.logAction(`Oh no! ${player.name} overshot!`);
-
-                newPosition = 100 - (newPosition - 100);
-            }
-
-            if (this.snakes[newPosition]) {
-                this.logAction(`Oh no! ${player.name} landed on a snake and slides to ${this.snakes[newPosition]}`);
-
-                this.game.players[playerIndex].currentPosition = this.snakes[newPosition];
-                this.game.players[playerIndex].totalSnakes++
-            } else {
-                this.logAction(`${player.name} is now on ${newPosition}`);
-
-                this.game.players[playerIndex].currentPosition = newPosition;
-            }
-
-            if (roll === 6) {
-                this.logAction(`${player.name} rolled a six and gets another turn!`);
-
-                this.handlePlayerTurn(playerIndex);
-            }
+            this.handlePlayerNotOnBoard(playerIndex, roll);
+            return;
         }
+
+        this.handlePlayerOnBoard(playerIndex, roll);
+    }
+
+    private handlePlayerNotOnBoard(playerIndex: number, roll: number): void {
+        const player = this.game.players[playerIndex];
+
+        if (roll === 6) {
+            this.logAction(`${player.name} is on board!`);
+
+            this.game.players[playerIndex].currentPosition = 1
+            this.game.players[playerIndex].isOnBoard = true
+        } else {
+            this.logAction(`${player.name} failed to get on board`);
+        }
+    }
+
+    private handlePlayerOnBoard(playerIndex: number, roll: number): void {
+        const player = this.game.players[playerIndex];
+
+        this.logAction(`${player.name} was on ${player.currentPosition}, will move to ${player.currentPosition + roll}`)
+
+        let newPosition = player.currentPosition + roll;
+        this.game.players[playerIndex].totalSteps += roll;
+
+        if (newPosition === 100) {
+            this.handlePlayerWon(playerIndex);
+        }
+
+        newPosition = this.checkPlayerOvershot(playerIndex, newPosition);
+
+        this.movePlayer(playerIndex, newPosition);
+
+        if (this.game.gameIsOver === false && roll === 6) {
+            this.logAction(`${player.name} rolled a six and gets another turn!`);
+
+            this.handlePlayerTurn(playerIndex);
+        }
+    }
+
+    private handlePlayerWon(playerIndex: number): void {
+        this.logAction(`${this.game.players[playerIndex].name} has won!!!`);
+
+        this.game.players[playerIndex].currentPosition = 100;
+        this.game.gameIsOver = true;
+        this.game.winner = playerIndex;
+
+        return;
+    }
+
+    private checkPlayerOvershot(playerIndex: number, position: number): number {
+        if (position > 100) {
+            this.logAction(`Oh no! ${this.game.players[playerIndex].name} overshot!`);
+
+            position = 100 - (position - 100);
+        }
+
+        return position;
+    }
+
+    private movePlayer(playerIndex: number, newPosition: number): void {
+        if (this.snakes[newPosition]) {
+            this.handleSnake(playerIndex, newPosition);
+        } else {
+            this.moveToPosition(playerIndex, newPosition);
+        }
+    }
+
+    private moveToPosition(playerIndex: number, newPosition: number): void {
+        const player = this.game.players[playerIndex];
+
+        this.logAction(`${player.name} is now on ${newPosition}`);
+
+        this.game.players[playerIndex].currentPosition = newPosition;
+    }
+
+    private handleSnake(playerIndex: number, newPosition: number): void {
+        const player = this.game.players[playerIndex];
+
+        this.logAction(`Oh no! ${player.name} landed on a snake and slides to ${this.snakes[newPosition]}`);
+
+        this.game.players[playerIndex].currentPosition = this.snakes[newPosition];
+        this.game.players[playerIndex].totalSnakes++;
     }
 }
 
